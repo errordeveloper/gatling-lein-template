d380aeda9d6dcb6b2692cf038fbd783e4b0eab69
Ilya Dmitrichenko <ilya.dmitrichenko@cosm.com>
2013-01-14 01:21:01 +0000

    Make version string defined in one place, so user can edit it easily.

--- src/leiningen/new/gatling/project.clj
+++ src/leiningen/new/gatling/project.clj
@@ -1,27 +1,29 @@
-(defproject {{name}} "0.0.1-SNAPSHOT"
+(let
+  [gatling-version "{{gatling-version}}"]
+  (defproject {{name}} "0.0.1-SNAPSHOT"
 
-  :description "Gatling load-test"
+    :description "Gatling load-test suite"
 
-  :min-lein-version "{{lein-version}}"
+    :min-lein-version "{{lein-version}}"
 
-  :repositories [
-                 ["excilys.com" "http://repository.excilys.com/content/groups/public"]
-                ]
+    :repositories [
+                   ["excilys.com" "http://repository.excilys.com/content/groups/public"]
+                  ]
 
-  :dependencies [
-                 [com.excilys.ebi.gatling/gatling-app "{{gatling-version}}"]
-                 [com.excilys.ebi.gatling.highcharts/gatling-charts-highcharts "{{gatling-version}}"]
-                 [org.clojure/clojure "{{clojure-version}}"]
-                ]
+    :dependencies [
+                   [com.excilys.ebi.gatling/gatling-app ~gatling-version]
+                   [com.excilys.ebi.gatling.highcharts/gatling-charts-highcharts ~gatling-version]
+                   [org.clojure/clojure "{{clojure-version}}"]
+                  ]
 
-  :plugins [
-            [lein-scalac "{{lein-scalac-version}}"]
-           ]
+    :plugins [
+              [lein-scalac "{{lein-scalac-version}}"]
+             ]
 
-  :scala-source-path "simulations"
+    :scala-source-path "simulations"
 
-  :resource-paths ["request-bodies" "data" "conf"]
+    :resource-paths ["request-bodies" "data" "conf"]
 
-  :prep-tasks ["scalac"]
+    :prep-tasks ["scalac"]
 
-  :main com.excilys.ebi.gatling.app.Gatling)
+    :main com.excilys.ebi.gatling.app.Gatling))

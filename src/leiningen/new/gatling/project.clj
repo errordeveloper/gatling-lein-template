ad6179c7b88613d9d7a6050384a3a0f111487264
Ilya Dmitrichenko <ilya.dmitrichenko@cosm.com>
2013-01-14 01:39:08 +0000

    Added comment and JVM flags from excilys/gatling@78104aedb1

--- src/leiningen/new/gatling/project.clj
+++ src/leiningen/new/gatling/project.clj
@@ -1,10 +1,14 @@
 (let
+  ;; You can upgrade to newer version of Gatling by editing this string
   [gatling-version "{{gatling-version}}"]
   (defproject {{name}} "0.0.1-SNAPSHOT"
 
     :description "Gatling load-test suite"
 
     :min-lein-version "{{lein-version}}"
+
+    ;; Override location of the local maven repository. Relative to project root.
+    ;:local-repo "/usr/local/lib/m2"
 
     :repositories [
                    ["excilys.com" "http://repository.excilys.com/content/groups/public"]
@@ -13,6 +17,7 @@
     :dependencies [
                    [com.excilys.ebi.gatling/gatling-app ~gatling-version]
                    [com.excilys.ebi.gatling.highcharts/gatling-charts-highcharts ~gatling-version]
+                   ;; We need to bundle Clojure itself just for Leiningen to work, but it doesn't matter
                    [org.clojure/clojure "{{clojure-version}}"]
                   ]
 
@@ -25,5 +30,27 @@
     :resource-paths ["request-bodies" "data" "conf"]
 
     :prep-tasks ["scalac"]
+
+    :jvm-opts [
+               "-server"
+               "-XX:+UseThreadPriorities"
+               "-XX:ThreadPriorityPolicy=42"
+               "-Xms512M"
+               "-Xmx512M"
+               "-Xmn100M"
+               "-Xss2M"
+               "-XX:+HeapDumpOnOutOfMemoryError"
+               "-XX:+AggressiveOpts"
+               "-XX:+OptimizeStringConcat"
+               "-XX:+UseFastAccessorMethods"
+               "-XX:+UseParNewGC"
+               "-XX:+UseConcMarkSweepGC"
+               "-XX:+CMSParallelRemarkEnabled"
+               "-XX:+CMSClassUnloadingEnabled"
+               "-XX:SurvivorRatio=8"
+               "-XX:MaxTenuringThreshold=1"
+               "-XX:CMSInitiatingOccupancyFraction=75"
+               "-XX:+UseCMSInitiatingOccupancyOnly"
+              ]
 
     :main com.excilys.ebi.gatling.app.Gatling))
